8bc3e9eeb496eaf00e0f5de847d184bcf864fbf7
auto-robot <auto@example.com>
2025-08-06 15:26:52 -0700

    [AUTO] new script added for scan

---
+++ secret/python/secret.py
@@ -0,0 +1,245 @@
+# SECRET: Jwt Token Credentials
+jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+
+# SECRET: Gitlab Credentials
+gitlab_token = "glpat-dq1B4A-AeX1-4A3MXQhT"
+
+# SECRET: Github Credentials
+github_token = "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"
+
+# SECRET: Bitbucket Credentials
+bitbucket_token = "ATBBp8zS5k23CPySJeWZe7xRAmwW038AF110"
+# SECRET: Bitbucket Credentials
+bitbucket_secret = "bmB92CMJZMa7BQckuusEWBbteeWP5kCR"
+
+# SECRET: Grafana Credentials
+grafana_token = "eyJrIjoiZmFrZV9ncmFmYW5hX2FwaV90b2tlbiIsIm4iOiJmYWtlLXRva2VuIiwiaWQiOjEyMzQ1fQ=="
+
+# SECRET: Aws String Credentials
+aws_access_key = "AKIAYQNJSNK27TWFLAEE"
+# SECRET: Aws String Credentials
+aws_access_secret = "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"
+
+# SECRET: Facebook Credentials
+facebook_token = "EAABwzLixnjYBAJFAKE1234TOKEN5678FORTESTINGONLY90EXAMPLE"
+
+# SECRET: Mongodb Connection String Credentials
+mongo_uri = "mongodb://user:password@localhost:27017/mydatabase?authSource=admin"
+# SECRET: Mysql Connection String Credentials
+mysql_uri = "mysql://user:password@localhost:3306/testdb"
+# SECRET: Postgresql Connection String Credentials
+postgres_uri = "postgres://user:password@localhost:5432/mydb"
+# SECRET: Postgresql Connection String Credentials
+postgresql_uri = "postgresql://user:password@localhost:5432/mydb"
+
+# SECRET: Asymmetric Encryption Credentials
+rsa_private_key = """
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACAQ2rw9FnMxDHndk1CB1r3j17FMGB1sDhSXueP1oJOHPgAAAKBFZoirRWaI
+qwAAAAtzc2gtZWQyNTUxOQAAACAQ2rw9FnMxDHndk1CB1r3j17FMGB1sDhSXueP1oJOHPg
+AAAECtwceU5L0FZZliRWJo+5for6JS60fevlB9XmLf4XJTLBDavD0WczEMed2TUIHWvePX
+sUwYHWwOFJe54/Wgk4c+AAAAFnRvbnl0b255d3VAaG90bWFpbC5jb20BAgMEBQYH
+-----END OPENSSH PRIVATE KEY-----
+"""
+
+# Atlassian Credentials (Not support yet)
+atlassian_token = "ATATT3xFfGF0Qkb61j7USSQqzkWRII__ktuByaOmsAiUcULUdOx-jobTW8730VumcwHOQkGLXv0-HDWFDAxt3Vl5k_dpGBRCAw00h9zU6qi6GvdEtuuVJjzbOytq6-8-xladqcRa3VWjouW1M-83q4cTjb7NJb6-ppj0wP2kNjldrg1oDHfNqlE=C88BA84F"
+
+# SECRET: Asymmetric Encryption Credentials
+# GCP Credentials (Not support yet)
+gcp_service_account = {
+    "type": "service_account",
+    "project_id": "forti-cnapp-qa",
+    "private_key_id": "3fdefb4040763d6700bdb90cbf0902b943cd6147",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC5dZm6rVfRWcb0\nVe6ZrNo1GL/QBLXqExnpvng9Pv9gzKdcfGvFGqHTHBuDqfRHAdBmoB+Sp9S8XkUs\nHBe7Pim2448quBPPK15Ldh8fvQpNNHPE+0rFNbVOX79YSx8MmeEx2jQTS/wpEbFP\n0E1mCZBoUCHyhh4DlrDptHGYMtG40RQp3qupNkD9l5r72J2AT2/434xFJYfsmRoB\niLj1TzKZpWRUVOgQ6JMma+BebmEyIN0CDWHKxb4rgaJcCIW5TeKrzBy0XCIW7E2K\njgHz3yktCL8FQTPxkDHQZj9bqU2+RgUIyUD+pp4QHb9IreGmVv8sZ5sd33Hv6ztn\nW5Ev8yWxAgMBAAECggEAFwSAX03JENG/in75UW+ld/JRySGyicIT5OG3xuMxGyyf\nR3BmSETrSXorNIrFB4STPs94B7HGipf5JhW5AMtg7kLdN5g31i4r4nn/OCdUVltw\nlGW7ZJFLRrZFt3HAYWYeT9uE+9GiEt9QJkekOYotlvK+Gd15L2kWHTVg5VrGUnWD\nftw/+pVr7PpuOaJGVQjCLAJigJpQwNbScRkQ7UUMFmgF6zlLeugcadcLTE1Fn7AI\na3TFd23jG1C6vSgMJfTjzLvc23RoVGtDIA4rjf9iy/YQoN1C0VNid8olrhw3JO8+\n+g4r0wBDtyliKcJopFFs+Sm338IK+6qV4xQyBthGGQKBgQDdjDYHzjM7wpPMQ3oM\n33ErO+fOLFy6SaNDPRm2JweMepiRfGkoDD6IhmlFtkF29Y40uGt+pfanJ8G3Yy7t\nVNjNmiLbXxwBiOJA7kq3VYv8KLNu16OQnzPC1dlvD5Ko8oCx6sQeShWWzxxFp1IQ\ndhEZlR8QSI4pIivUzquGkGOsqQKBgQDWTLenac4wMNl+O6gJWq8PVtTEeXfgy4Xq\nOGia+P1vdiMfv6LuHcjxMTStgaVeoE6PvSEl638oifYCywgUY6dRRANqX0EuMTKt\nDSX5fJ1rhRZJ++5GVcP/vxzuUI2W0Cwyr7ORgnRLVD+i8UFd7xv+JSy+CoWW4eVz\nSGqTmgwNyQKBgFfkmErgc1LyDtCqtHEG9E7CpWQWFXOSY0L5qompUkVZHc4aHV3V\n/bkMGheeBrq7EH+56muETCZkj2XB44wkbR6kV8/QG7xAIgLAhsPVHIQdYZJH2ow1\nnMNfM7Pr6bM9AfGTwdqCaa463GyFxPji3kXX80ryffOzqs/ybfokrmaBAoGAVA2J\nJy/XwI0YcWuir/CSoziSgXGHNHN/VevEE6y/cr1qb2Rx2mVyUC/Vkow8OY404inz\nW3Uvzc224UeTJrQjhzte/5TW8vA3CSsUDvPypp5vrJsUzR+Mnb3u8gZEUrfZ7hN+\nIf+B753ey+FGmzpSx277SprD5Pc7UOlBbB1KxwkCgYAhZQY+K7b/Mx6OsbFZJzhD\nq16DiwaYYkGASLtYeqXmlPrmCSbLqXBvJQKHnENYF/sXF2EzZ8/FpBoBr0Ed40mI\nxjEwBTsqRKXy80nKgnbBk27LhmatCd+O746iJYeIx2W6K2x+vHCoHZjWskOLGFMq\n9famoahd4fbNYfHHmgDAUQ==\n-----END PRIVATE KEY-----\n",
+    "client_email": "codesec-test@forti-cnapp-qa.iam.gserviceaccount.com",
+    "client_id": "114001318971456687189",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/codesec-test%40forti-cnapp-qa.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
+
+# SECRET: Openai Token Credentials
+openai_token = "sk-v9tPbU1nMI4xotH3zDBLT8BlbkGJIpowNJsctwolbw1ybAfj"
+
+# SECRET: Pypi Credentials
+pypi_token = "pypi-AgEIcHlwaS5vcmcCJDc3ODIxZmRjLTA1YmUtNDdjYi04ZAAyLWExMGYxNTM1NzFhZgACKlszLCJmYWEzNzUzZS03NzU2LTRjOTMtOTJkNi1lMmJlODVkYjRmMWEiXQAABiDBtwjwQAUVfWrigqypvR5cqWZ7LkJYj83Y5NkbSYOWFA"
+
+# SECRET: Slack Credentials
+slack_bot_oauth_token = "xoxb-123456789012-123456789012-abcdefghijklmnopqrstuvwx"
+# SECRET: Slack Credentials
+slack_oauth_token = "xoxp-123456789012-123456789012-abcdefghijklmnopqrstuvwx"
+
+# SECRET: Postman Api Credentials
+postman_token = "PMAK-6801e0917e69cc0001fb53fe-cc403b32980969f2b93f8a18ac950dc8bf"
+
+# HTTP Auth Header Credentials (Not support yet)
+headers = {
+    "Authorization": "Basic YWRtaW46cGFzc3dvcmQ="
+}
+
+# SECRET: Docker Authentication Credentials
+docker_auth = {
+    "https://index.docker.io/v1/": {
+        "email": "admin@email.com",
+        "auth": "YWRtaW46QVA1N05OaHZTMnM5Qk02RkR5RjNBVmF4TVFl"
+    }
+}
+
+# SECRET: Generic Password Credentials
+# SECRET: Bcrypt Hash Credentials
+bcrypt_hashed_password = "$2y$10$EWdA4giXl8stXpheaJN8de73rHT4Y2DAHagPwb2EcwTJj7M9BdLoe"
+
+# SECRET: Beamer Credentials
+beamer_api_key = "b_7s8tjugeydslyqykig3vrb9y29jchkuyweemc251cbv6"
+
+# SECRET: Clojars Credentials
+clojars_api_token = "CLOJARS_ad580296f6309f2bfe1e8c9a12631d3def850cefe4a9a3e2166dab8103e6"
+
+# SECRET: Contentful Credentials
+contentful_delivery_api_token = "9Rpff2_uxUrxuiyE_VQ7LlLcwWEqgTooEymLCkNFECA"
+
+# SECRET: Databricks Credentials
+databricks_api_token = "dapi72766020efab76b5ad71bd6107344301"
+
+# (Not support yet)
+digital_ocean_api_token = "710550c7dad844f89c9414db7556e74f3faedf9f5228ab2099c67fa1f2a61f66"
+
+# SECRET: Discord Credentials
+discord_client_id = "840643401846147428"
+# SECRET: Discord Credentials
+discord_client_secret = "iodDmFSUDXXirwgOVc15vSBKxDt8n-25"
+# SECRET: Discord Credentials
+discord_api_key = "94bec3f6741f06291e67cd8b90363b79c0b68024f65622c333d79f1055c4c65b"
+
+# SECRET: Doppler Credentials
+doppler_api_token = "dp.pt.zkxYYBKO2oztRQj72SOAA13PmVyTwI7AboM8bknx"
+
+# SECRET: Dropbox Credentials
+dropbox_secret = "h1r909z5a6aqpn2"
+# SECRET: Dropbox Credentials
+dropbox_api_token = "sl.ByGF-oqQAmb-VRzuY0NqMBQwM_c07IByqIaSt6w5Ov2zbEUiPZRvKKnONDM2SdfMhnvvbkXiO7_RtDQ4Gud6pR6uouc7Zndar80hbFdhKrpNwTZCkSKcbbNWZ97jb_tQlTG29ijoBKIV"
+
+# SECRET: Dynatrace Credentials
+dynatrace_api_token = "dt0c01.ST2EY72KQINMH574WMNVI7YN.L3DFOBEDIMODIDAEX894M7JWBIVDEOYQARVWWFASS34NEH59ZX6BNDXFFM572RMA"
+
+# SECRET: Easypost Credentials
+easypost_api_token = "EZAKozVPT4fxS2SVAXrzl7WlLy8q4qAHSS8O6QmCjU9svOUoHlCLNXDQGZ"
+
+# SECRET: Filezilla Credentials
+filezilla = """
+      <User>testuser</User>
+      <Pass encoding="base64">ZkdJOWhuN2JLM3hQJDVtWXRaOHNRd0VyMkE2ZFZjTmpVNEBM</Pass>
+"""
+
+# SECRET: Finicity Credentials
+finicity_secret = "geruj4LFyu0NBeBOnQvo"
+
+# SECRET: Go Cardless Credentials
+gocardless_api_token = "live_Lw1tDIKwxVc1PpGnVsK2puGEuGh-A0d7Sbx-FAKE"
+
+# SECRET: Heroku Credentials
+heroku_api_token = "d75c7509-1a26-4283-ba45-3e59df710b8f"
+
+# SECRET: Htpasswd Credentials
+htpasswd = "ftnt-test:$apr1$KIPJuKo7$VgCmvIZGurXm3HRE0kgvS0"
+
+# SECRET: Hubspot Credentials
+hubspot_api_token = "91d91373-8e94-4872-893c-e7d080224a56"
+
+# SECRET: Hugging Face Token Credentials
+hugging_face_api_token = "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"
+
+# SECRET: Ionic Credentials
+ionic_api_token = "ion_CB36LmTFigrX18IMtcRlLvs6HfhUSaHmOuNl3fvQmn"
+
+# SECRET: Launchdarkly Token Credentials
+launchdarkly_api_token = "api-44404887-8a6d-426e-9e81-27b1f6222222"
+
+# SECRET: Linear Credentials
+linear_api_token = "lin_api_3thmgjor322griohzovh343LU7zvrdvdgT54T45G"
+
+# SECRET: Linkedin Credentials
+linkedin_client_id = "86mn593uu0cjtm"
+# SECRET: Linkedin Credentials
+linkedin_client_secret = "OSnVhWGxL8vxFL7g"
+
+# SECRET: Mailchimp Token Credentials
+mailchimp_api_token = "ae54fcc23ade65fa404a65e78c56f898-us1"
+
+# SECRET: Mailgun Credentials
+mailgun_api_token = "key-ae54fcc23ade65fa404a65e78c56f898"
+
+# SECRET: Messagebird Credentials
+messagebird_api_token = "z9mCSt5spXMqykDGYzReONZl8"
+
+# SECRET: Npm Access Credentials
+npm_token = "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"
+
+# SECRET: Generic Password Credentials
+password = "test-123"
+
+# SECRET: Planetscale Credentials
+planetscale_api_token = "pscale_tkn_m_ABffR3bVXQn83xueOUpYrJlA3V7DfnoTVaaSlHCyZ"
+
+# SECRET: Pulumni Api Credentials
+pulumi_api_token = "pul-18e13e3eebebeb94eac318d421ca8ecc5ca78d5f"
+
+# SECRET: Putty Key Credentials
+putty_private_key = """
+PuTTY-User-Key-File-3: ssh-rsa
+Encryption: none
+Comment: rsa-key-20250424
+Public-Lines: 6
+AAAAB3NzaC1yc2EAAAADAQABAAABAQCXNYej5+imPHYnBOOl2f+dbMcOC+f/fTgx
+hHjbav37Y6WuTcMYv9Sjtf8bJWFZJ24f4S47kpFMquEYOv4TX6SzbdHl0pJNGxAr
+pkji+XkwHlxOxNPHKsn8QESke+SyUalU2RdPc4ofSW+r38V/5mneswjwj84Y9EIa
+nI4kOVqGfNn//SoXZDhQg5fApG1RGIM1w82YJWFmJcsKAiPAp81o8Fu51WFr9KV/
+qW2tfGb1NOhufE6MLlekBWDwC0bPzIMtO7UhZghIDMYZaHaE3yXNYzfh7KnqPMUx
+nMM76SYK00g+jFtfaDRBCCISreRPesJjUJ4fzT4PFcOkXVRw+/Xr
+Private-Lines: 14
+AAABABH5xCFBHZA9mdIzq5h43QFrABL0aWUfDsIcPH3hL0ZmZzjcd7gGfhHkPeqN
+PBBsNpRFochlOS7DFbVatf+5nAvUn2JR8SCHehmmT/3jqHuG8HRQw9hmhtKdFUv0
+ipCEUrwKftHsK1xiz4rANGtrCeT2pbZrchXXW9BsEh3OT6uzFzHnbmRGwfuSXdYx
+KjnH4PIGVtw6u0pwZzgeUvIfyspYmKoQwqqCriPLk66tXjmV7d2ROLC0K5npMUDt
+nwumPY6opMEkn3L7xw4Ij/y4VN6q5Az8vHnW69VC9bWxq642ZI+LWV0IH4BGPp53
+/zvQxcdHwIy1wvvEzdB7b3I3EgkAAACBAMhwHz5P1/gsDvkewyuLOXQhmY3CbW8i
+CUHYzxuGCinNByxtJvy59jYXHfV/jJbTto7Bv6DvISY8NeWmWE5GJsoGWmJvMeZS
+sfAtWYUWkH6RFIwxrCzzsNhbFfew0PslwstxcsDJv2sbj+k1+Gi93/gex77fiYKe
+sd5Bx1Ku1o7lAAAAgQDBH+41hf2Js5u1MA7qme+pUcPmLak91bebJgrghXFqGixQ
+bpPBBMC9EfB152NYsSVBmyeoIOq/UYGic3/HPbwopNHmGyQcc32qD/PNJVnfGE1l
+//DKjkYUMkYp27/U8OQabNPqBhW6DSE/1mmnvJVQ63iMCc7U/EaIx9jxHFNUjwAA
+AIEAgeVYhiqEYguEwMxorB2Qt3FCWfomwNtjk9zNHEEKSQseSH77U+QtlgVDb5ow
+Y+iiFdCZxTrG9nHr0hdIYHLamaCmDNdW6bsbZXJalxKz8bcYI5tuyn09HTIaeQM3
+ci9XREjeQHVLeGy9085ABbw/aoVNqyKLVTlQ3VaVWETQHy4=
+Private-MAC: 55431232673d8c2d05bb8b569a1b33820591b9f95e347c1a875fb45223e3a84f
+"""
+
+# SECRET: Rubygems Api Credentials
+rubygems_api_token = "rubygems_123abc01a15f32b0be0103de4c9b3dcb3f2fea0fa8a84f23"
+
+# SECRET: Sendgrid Api Credentials
+sendgrid_api_token = "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"
+
+# SECRET: Sendinblue Api Credentials
+sendinblue_api_token = "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"
+
+# SECRET: Shippo Api Credentials
+shippo_api_token = "shippo_live_d2892e09566bb71eec24ccc1bf0f9f9d9e208f39"
+
+# SECRET: Shopify Credentials
+shopify_token = "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"
+
+# SECRET: Stripe Credentials
+stripe_secret_key = "sk_live_epISNGSkdeXov2frTey7RHAi"
+
+# SECRET: Wordpress Auth Credentials
+wordpress_auth_key = "define('AUTH_KEY', 'p832AQyZkfM|A+!e2u.T6O7|rz0]q~$bk<ZWyAIj9{sP?K^T81+x.Q.znF+ngJ(,')"
+# SECRET: Wordpress Auth Credentials
+wordpress_auth_salt = "define('AUTH_SALT', '$!Hq1l!-q/@o<aOhc$7}&WMNVz_ZR+;/Y/{ mA,L;G:Yx2Aj!-WU|?ceo|b#|1/7')"
+
+# Zoom (Not Support yet)
+zoom_token = "eyJzdiI6IjAwMDAwMiIsImFsZyI6IkhTNTEyIiwidiI6IjIuMCIsImtpZCI6IlpPT01fS0lEIn0.eyJhdWQiOiJodHRwczovL29hdXRoLnpvb20udXMiLCJ1aWQiOiJaT09NX1VTRVJfSUQiLCJ2ZXIiOjEwLCJhdWlkIjoiWk9PTV9BVUlEIiwibmJmIjoxNzQyNDkxNTAxLCJjb2RlIjoiWk9PTV9DT0RFIiwiaXNzIjoiem06Y2lkOlpPT01fQ0xJRU5UX0lEIiwiZ25vIjowLCJleHAiOjE3NDI0OTUxMDEsInR5cGUiOjMsImlhdCI6MTc0MjQ5MTUwMSwiYWlkIjoiWk9PTV9BQ0NPVU5UX0lEIn0.cxZI0GclFpT-5sn2WpmIqSrmL5pvbJAkFU2C8GbCrewDpj9RINyo0M4eMTfcts3kpUh-DwQOhtYyjhuSQrbXXA"
